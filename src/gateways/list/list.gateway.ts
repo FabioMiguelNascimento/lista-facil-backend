c40b4babf0f3422d839f082f260dc99bed7219d0
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2026-02-15 09:32:05 -0300

    fix: update CORS origin to use environment variable for frontend URL

--- src/gateways/list/list.gateway.ts
+++ src/gateways/list/list.gateway.ts
@@ -10,7 +10,7 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL!,
     credentials: true,
   },
 })

40f46d5640f346be66fd3931c814e4511b7ed690
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2026-02-15 14:15:57 -0300

    fix: reorder module imports for improved readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
-import { ListsModule } from './modules/lists/lists.module';
 import { InvitesModule } from './modules/invites/invites.module';
-import { ListGateway } from './gateways/list/list.gateway';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './modules/users/users.module';
 import { ItemsModule } from './modules/items/items.module';
+import { ListsModule } from './modules/lists/lists.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, ListsModule, InvitesModule, PrismaModule, UsersModule, ItemsModule],
